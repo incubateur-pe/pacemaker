b9c0910b28f59fbdebb02572cff94e32cd472a69
ulrich giraud <ulrich.giraud1@gmail.com>
2021-03-05 22:32:51 +0100

    feat: add debian 10 compatibility

--- molecule/common/env_ugi_distant.sh
+++ molecule/common/env_ugi_distant.sh
@@ -1,5 +1,6 @@
 REMOTE_HOST=true
 CENTOS_BASE_MIRROR=http://10.0.4.40:8081/repository/centos-base/centos
+DEBIAN_PROXY=http://10.0.4.40:8081/repository/debian-proxy
 PROVIDER_ARGS=['host="kvm"', 'username="libvirt-user"']
 SSH_CONFIG=ssh_config_ugi
 SSH_ARGS=-o ProxyCommand="ssh -W %h:%p -q -l libvirt-user kvm"
